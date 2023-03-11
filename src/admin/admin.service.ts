3bdbd7ec181ae7e39ec5ef60e18ee1ccbfe3be87
Jonathan Bustos <jonathanbust@gmail.com>
2023-03-11 11:35:55 -0300

    feat(administrador modulos): :memo: se agega los modulos de loginadmin y administrador
    haciendo relaciones a base de datos, al modulo admin

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Landing } from '../entities/landing.entity';
+
+
+
 
 @Injectable()
-export class AdminService {}
+export class LandingService {
+    constructor(
+        @InjectRepository(Landing) 
+        private landingRepo: Repository<Landing> 
+    ) {}
+    
+    //busca todo get 
+    findAll(){
+        this.landingRepo.find();
+    }
+    //busca de forma individual get por id 
+    findOne(id: number): Promise<Landing> {
+        return this.landingRepo.findOneBy({id:id});
+    }
+    //crea el post
+    create(body: any) {
+        const newTask = this.landingRepo.create(body);
+        
+        return this.landingRepo.save(newTask);
+    }
+    //actualiza por id(put)
+    async update(id: number, body: any){
+        const landing = await this.landingRepo.findOneBy({id:id});
+        this.landingRepo.merge(landing, body);
+        return this.landingRepo.save(landing)
+    }
+    //borra por id (delete)
+    async delete(id: number) {
+        await this.landingRepo.delete(id);
+        return true;Þ
+    }
+}
